80ffb1a4e7bec0e20221e72fab7333b1ff5977a0
chengyiju <bypass7492@gmail.com>
2021-04-28 20:46:51 +0800

    p

---
+++ day 03/bmi 4.py
@@ -0,0 +1,23 @@
+import  math
+
+def getBMI(h ,w):
+    bmi = w / math.pow (h/100, 2)
+    return bmi
+
+def getResult(bmi)
+    result ="過輕"
+    if 18 < bmi <= 23:
+        result
+def getBMIAndResult(h, w)
+    bmi = getBMI(h ,w)
+    return result
+
+
+bmi1 = getBMI(170, 60)
+bmi2 =getBMI(180 ,90)
+print(bmi1 ,bmi2)
+
+bmi3 , result = getBMIAndResult(170, 60)
+bmi4 , result = getBMIAndResult(170, 60)
+print(bmi3 , result3)
+print(bmi4 ,result4)
