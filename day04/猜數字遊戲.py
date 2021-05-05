b03852ecc0bb74c33b8eba955a62b847c3491e19
chengyiju <bypass7492@gmail.com>
2021-05-05 19:59:02 +0800

    p

--- day04/猜數字遊戲.py
+++ day04/猜數字遊戲.py
@@ -1,16 +1,21 @@
 import  random as r
+ans = r.randint(1 ,99)
 ans = 47
 min = 0
 max = 100
-
-
+count = 5
 while True:
-    guess = ('請輸入 %d~%d:' % (min ,max))
-    if guss > ans:
+    guess = int(input('請輸入 %d~%d:' % (min ,max))
+    # 檢查 guess 的資料是否在min 與 max 之間?
+    if guess <= min or guess >= max:
+        print('數字範圍錯誤')
+        continue
+    # 判定結果
+    if guess > ans:
         max = guess
     elif guess < ans:
         min = guess
     else:
         print('答對了')
-        break;
+        break
 
